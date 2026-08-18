aba0193b6d906700d56f956e1254d30140f3c2aa
Alexandra C - RoofingDirect <161867665+alexandrac02@users.noreply.github.com>
2026-08-18 10:16:33 -0600

    Update Classes for notifications page
    - Update: Any class names beginning with "4wp" to "forwp" to mimic other areas in the plugin
    (current shortcode css was broken due to starting the class name with a digit)

--- includes/class-list-renderer.php
+++ includes/class-list-renderer.php
@@ -61,13 +61,13 @@ class ForWP_Notifications_List_Renderer {
 		$rest_url      = rest_url( ForWP_Notifications_REST_Controller::NAMESPACE );
 		$nonce         = wp_create_nonce( 'wp_rest' );
 		$poll_interval = 30000;
-		$empty_text    = __( 'No notifications.', '4wp-notifications' );
+		$empty_text    = __( 'No notifications.', 'forwp-notifications' );
 
 		ob_start();
 		?>
-		<div class="4wp-notifications" data-forwp-poll="1" data-forwp-rest-url="<?php echo esc_url( $rest_url ); ?>" data-forwp-nonce="<?php echo esc_attr( $nonce ); ?>" data-forwp-poll-interval="<?php echo esc_attr( (string) $poll_interval ); ?>" data-forwp-empty-text="<?php echo esc_attr( $empty_text ); ?>">
+		<div class="forwp-notifications" data-forwp-poll="1" data-forwp-rest-url="<?php echo esc_url( $rest_url ); ?>" data-forwp-nonce="<?php echo esc_attr( $nonce ); ?>" data-forwp-poll-interval="<?php echo esc_attr( (string) $poll_interval ); ?>" data-forwp-empty-text="<?php echo esc_attr( $empty_text ); ?>">
 			<?php if ( ! empty( $items ) ) : ?>
-				<ul class="4wp-notifications__list">
+				<ul class="forwp-notifications__list">
 					<?php
 					foreach ( $items as $item ) {
 						self::render_item( $item );
@@ -75,7 +75,7 @@ class ForWP_Notifications_List_Renderer {
 					?>
 				</ul>
 			<?php else : ?>
-				<p class="4wp-notifications__empty"><?php echo esc_html( $empty_text ); ?></p>
+				<p class="forwp-notifications__empty"><?php echo esc_html( $empty_text ); ?></p>
 			<?php endif; ?>
 		</div>
 		<?php
@@ -89,22 +89,22 @@ class ForWP_Notifications_List_Renderer {
 		$is_read         = ( (int) $item['is_read'] ) === 1;
 		$source          = isset( $item['source'] ) ? $item['source'] : '';
 		$item_icon_class = ForWP_Notifications_Bell_Renderer::get_item_icon_class( $source );
-		$toggle_label    = $is_read ? __( 'Mark as unread', '4wp-notifications' ) : __( 'Mark as read', '4wp-notifications' );
+		$toggle_label    = $is_read ? __( 'Mark as unread', 'forwp-notifications' ) : __( 'Mark as read', 'forwp-notifications' );
 		$toggle_icon     = $is_read ? 'dashicons-hidden' : 'dashicons-visibility';
-		$toggle_class    = '4wp-notifications__toggle' . ( $is_read ? ' 4wp-notifications__toggle--read' : '' );
+		$toggle_class    = 'forwp-notifications__toggle' . ( $is_read ? ' forwp-notifications__toggle--read' : '' );
 		?>
-		<li class="4wp-notifications__item <?php echo $is_read ? 'is-read' : ''; ?>" data-id="<?php echo esc_attr( (string) $item['id'] ); ?>">
-			<span class="4wp-notifications__item-icon" aria-hidden="true"><span class="dashicons <?php echo esc_attr( $item_icon_class ); ?>"></span></span>
-			<div class="4wp-notifications__content">
+		<li class="forwp-notifications__item <?php echo $is_read ? 'is-read' : ''; ?>" data-id="<?php echo esc_attr( (string) $item['id'] ); ?>">
+			<span class="forwp-notifications__item-icon" aria-hidden="true"><span class="dashicons <?php echo esc_attr( $item_icon_class ); ?>"></span></span>
+			<div class="forwp-notifications__content">
 				<?php if ( ! empty( $item['payload']['title'] ) ) : ?>
-					<span class="4wp-notifications__title"><?php echo esc_html( $item['payload']['title'] ); ?></span>
+					<span class="forwp-notifications__title"><?php echo esc_html( $item['payload']['title'] ); ?></span>
 				<?php endif; ?>
 				<?php if ( ! empty( $item['payload']['message'] ) ) : ?>
-					<p class="4wp-notifications__message"><?php echo esc_html( $item['payload']['message'] ); ?></p>
+					<p class="forwp-notifications__message"><?php echo esc_html( $item['payload']['message'] ); ?></p>
 				<?php endif; ?>
-				<span class="4wp-notifications__date"><?php echo esc_html( date_i18n( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ), strtotime( $item['created_at'] ) ) ); ?></span>
+				<span class="forwp-notifications__date"><?php echo esc_html( date_i18n( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ), strtotime( $item['created_at'] ) ) ); ?></span>
 				<?php if ( ! empty( $item['payload']['url'] ) ) : ?>
-					<a class="4wp-notifications__link" href="<?php echo esc_url( $item['payload']['url'] ); ?>" aria-label="<?php esc_attr_e( 'Go to page', '4wp-notifications' ); ?>"><span class="4wp-notifications__link-icon dashicons dashicons-external" aria-hidden="true"></span></a>
+					<a class="forwp-notifications__link" href="<?php echo esc_url( $item['payload']['url'] ); ?>" aria-label="<?php esc_attr_e( 'Go to page', 'forwp-notifications' ); ?>"><span class="forwp-notifications__link-icon dashicons dashicons-external" aria-hidden="true"></span></a>
 				<?php endif; ?>
 			</div>
 			<button type="button" class="<?php echo esc_attr( $toggle_class ); ?> forwp-js-toggle" data-id="<?php echo esc_attr( (string) $item['id'] ); ?>" data-is-read="<?php echo $is_read ? '1' : '0'; ?>" aria-label="<?php echo esc_attr( $toggle_label ); ?>"><span class="dashicons <?php echo esc_attr( $toggle_icon ); ?>" aria-hidden="true"></span></button>
